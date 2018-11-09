9e5310983b7bf94160803ab433876e371e38972e
Stijn Geens <stijn.geens@outlook.be>
2018-11-09 08:53:14 +0100

    @Stateless aan alle services toegevoegd

--- src/main/java/be/thomasmore/travelmore/service/BusService.java
+++ src/main/java/be/thomasmore/travelmore/service/BusService.java
@@ -2,8 +2,10 @@ package be.thomasmore.travelmore.service;
 
 import be.thomasmore.travelmore.repository.BusRepository;
 
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+@Stateless
 public class BusService {
     @Inject
     private BusRepository busRepository;
