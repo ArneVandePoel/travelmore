6aa4886a2fac66db8976094cc8e0fc1690c29352
Wim Naudts <wimnaudts@yahoo.com>
2018-11-09 18:29:26 +0100

    Werkende navigatie & doorsturen data naar detailpagina

--- src/main/java/be/thomasmore/travelmore/controller/ReisController.java
+++ src/main/java/be/thomasmore/travelmore/controller/ReisController.java
@@ -4,25 +4,38 @@ import be.thomasmore.travelmore.domain.Reis;
 import be.thomasmore.travelmore.service.ReisService;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 import javax.inject.Inject;
 import java.util.List;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class ReisController {
     @Inject
     private ReisService reisService;
+
+    //reis voor op detailpagina
+    private Reis reis;
+
+    //reis ophalen
+    public Reis getReis(){
+        return this.reis;
+    }
 
     public List<Reis> getReizen() {
         return this.reisService.findAllReizen();
     }
 
-    public Reis getReis(int id) {return this.reisService.findReis(id);}
-
     public  String[] getExtras(int id){
         Reis reis = this.reisService.findReis(id);
         String[] extras = reis.getHotel().getExtra().split(";");
         return extras;
+    }
+
+    public String getReisDetail(int id){
+        this.reis = this.reisService.findReis(id);
+
+        return "detail";
     }
 }
